51eebc23469daca72fe571c316cf65516b0a3888
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-02-25 23:39:28 -0500

    modelo producto

--- TopChoice/Productos/models.py
+++ TopChoice/Productos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from Categorias.models import Category
+from django.urls import reverse
 
-# Create your models here.
+class Productos(models.Model):
+    product_id = models.IntegerField(unique=True,primary_key=True)
+    product_name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=100,unique=True)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    product_image_stock = models.ImageField(upload_to='photos/products/stock')
+    product_image_secundary = models.ImageField(upload_to='photos/products/secundary')
+    product_image_extra = models.ImageField(upload_to='photos/products/extra')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    
+    def get_url(self):
+        return reverse('product_information',args=[self.category.slug,self.slug])
+    
+    def __str__(self):
+        return self.product_name
