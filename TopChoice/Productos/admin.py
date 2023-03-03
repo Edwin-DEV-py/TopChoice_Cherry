858732b664d864dbb551fed202d06c9c09c69963
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-03-02 21:24:03 -0500

    llenado DB1, cambios modelo products

--- TopChoice/Productos/admin.py
+++ TopChoice/Productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class Product_admin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('product_name',)}
+    list_display = ('product_name','price','stock','is_available','category','subcategory')
+  
+admin.site.register(Products,Product_admin)  
--- TopChoice/Productos/models.py
+++ TopChoice/Productos/models.py
@@ -3,14 +3,14 @@ from Categorias.models import Category,SubCategory
 from django.urls import reverse
 
 class Products(models.Model):
-    product_id = models.IntegerField(unique=True,primary_key=True)
+    product_id = models.AutoField(unique=True,primary_key=True)
     product_name = models.CharField(max_length=50)
     slug = models.CharField(max_length=100,unique=True)
     description = models.CharField(max_length=100)
     price = models.IntegerField()
     product_image_stock = models.ImageField(upload_to='photos/products/stock')
-    product_image_secundary = models.ImageField(upload_to='photos/products/secundary')
-    product_image_extra = models.ImageField(upload_to='photos/products/extra')
+    product_image_secundary = models.ImageField(upload_to='photos/products/secundary',null=True,blank=True)
+    product_image_extra = models.ImageField(upload_to='photos/products/extra',null=True,blank=True)
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
