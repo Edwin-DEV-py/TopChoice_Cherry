581747a63fc4dc4a13f0ab69034b08ff65d646f3
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-03-14 22:04:06 -0500

    Collectstatic y avances en el header y home

--- TopChoice/TopChoice/urls.py
+++ TopChoice/TopChoice/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.header, name="header")
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
