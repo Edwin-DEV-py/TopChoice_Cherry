8fd5f16d7b166499002aa29ee117b6b16218f3cf
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-03-27 00:10:06 -0500

    carrito

--- TopChoice/Carrito/models.py
+++ TopChoice/Carrito/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from Productos.models import *
 from Usuario.models import *
 
+#modelo que representa al carrito de compra
 class Cart(models.Model):
     cart_id = models.AutoField(unique=True,primary_key=True)
     date_added = models.DateTimeField(auto_now_add=True)
     
+    #el metodo str le indica a Django que imprimir cuando se necesita imprimir una isntancia de un modelo
     def __str__(self):
         return self.cart_id
-    
+
+#modelo que representa los elementos del carrito 
 class Cart_item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
