64db5559d21480250edb90a6df08e5dca848c84f
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-03-26 23:26:13 -0500

    primera vista del carrito de compra

--- TopChoice/TopChoice/urls.py
+++ TopChoice/TopChoice/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('inicio_sesion.html',views.inicio_sesion,name="inicio_sesion"),
     path('registro.html',views.registro,name="registro"),
     path('store/',include('Productos.urls')),
+    path('shopping_cart/',include('Carrito.urls')),
     path('inventario.html',views.inventario,name="inventario")
     #aqui colocar la etiqueta include para usar las urls de las otras apps.
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
