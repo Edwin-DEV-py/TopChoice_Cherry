71fdda7b19d572afef00fe6dd23ab0aaf6cedf6a
edwin-tarazona <80715680+Edwin-Papasito@users.noreply.github.com>
2023-03-30 00:22:39 -0500

    anadir productos al cart

--- TopChoice/Carrito/views.py
+++ TopChoice/Carrito/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render,redirect
+from django.core.exceptions import ObjectDoesNotExist
 from Productos.models import *
 from .models import *
 
 #obtenemos la sesion del usuario actual dentro de la web local a traves de una funcion que trabaja a nivel de archivo
 def _cart_id(request):
+    
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
     return cart
     
 def add(request, product_id):
+    
     product = Products.objects.get(product_id=product_id)
     
     #verificamos si el carrito existe y si no lo creamos
@@ -36,7 +39,28 @@ def add(request, product_id):
     
     return redirect('shopping_cart')
     
-
-def shopping_cart(request):
+def shopping_cart(request, total=0, quantity=0, items=None):
     
-    return render(request, 'tienda/carrito.html')
+    #mostramos los productos si existen
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))#obtenemos el carrito asignado
+        items = Cart_item.objects.filter(cart=cart, is_active=True) #esto es una lista de los items
+        #bucle para allar el precio total de la orden
+        for item in items:
+            total += (item.product.price*item.quantity)
+            quantity += item.quantity
+            
+        iva = (5*total)/100
+        final = total + iva
+    except ObjectDoesNotExist:
+        pass
+    
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'items':items,
+        'iva':iva,
+        'final':final
+    }
+    
+    return render(request, 'tienda/carrito.html',context)
