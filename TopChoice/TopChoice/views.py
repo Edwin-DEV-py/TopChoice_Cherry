311949a8eacbedee918f7bc55707728ecd6cc2d1
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-04-16 01:47:12 -0500

    pagina de inicio

--- TopChoice/TopChoice/views.py
+++ TopChoice/TopChoice/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from Categorias.models import Category, SubCategory
+from Productos.models import Products
 
 #vista para la pagina principal
 def header(request):
     categorias = Category.objects.all()
+    productos_top1 = Products.objects.filter(is_available=True)[30:34]
+    productos_top2 = Products.objects.filter(is_available=True)[40:44]
+    productos_new1 = Products.objects.filter(is_available=True).order_by('-date_created')[:4]
     context = {
         'categorias':categorias,
+        'productos_top1':productos_top1,
+        'productos_top2':productos_top2,
+        'productos_new1':productos_new1
     }
     return render(request,'index.html',context)
 
